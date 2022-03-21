1f1ab7e1f7075dc0516f9a812db383f8fd2c8148
Elizabeth Norton <epn013@hotmail.com>
2022-03-21 16:09:20 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+class Workout {
+    constructor(name){
+        this.name = name;
+        this.exercise = [];
+    }
+}
+
+class Exercise {
+    constructor(name, reps){
+        this.name = name;
+        this.reps = reps;
+    }
+}
+
